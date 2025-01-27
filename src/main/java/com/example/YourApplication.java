c16da09afdfa4e06cb5709a6d70964c522c61ad7
Vlad Barbu <vlad.barbu@tevvo.com>
2025-01-27 20:52:16 +0200

    132

---
+++ src/main/java/com/example/YourApplication.java
@@ -0,0 +1,13 @@
+public package src.main.java.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YourApplication.class, args);
+    }
+} {
+    
+}
